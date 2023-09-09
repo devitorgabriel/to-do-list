04f613fd7753fd750e2efdd9538b524816a0f371
devitorgabriel <139719006+devitorgabriel@users.noreply.github.com>
2023-09-09 15:18:11 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/pages/home/home.component';
 
 const routes: Routes = [
-  {path: "", component: HomeComponent}
+  {path: "https://devitorgabriel.github.io/to-do-list/", component: HomeComponent}
 ];
 
 @NgModule({
